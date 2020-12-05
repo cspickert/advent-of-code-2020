e98a1ca822537f41562b3e14306b6080b59c535f
Cameron Spickert <cameron@checkhq.com>
2020-12-05 13:16:11 -0500

    Add day 2

--- day01.py
+++ day01.py
@@ -1,5 +1,4 @@
-def day01_01(f):
-    nums = [int(line) for line in f]
+def part1(nums):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == 2020:
@@ -7,8 +6,7 @@ def day01_01(f):
                 return
 
 
-def day01_02(f):
-    nums = [int(line) for line in f]
+def part2(nums):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             for k in range(j + 1, len(nums)):
@@ -18,6 +16,8 @@ def day01_02(f):
 
 
 if __name__ == "__main__":
-    for fn in [day01_01, day01_02]:
-        with open("input/day01.txt") as f:
-            fn(f)
+    from input import day01
+
+    nums = [int(line) for line in day01.split()]
+    part1(nums)
+    part2(nums)
---
+++ day02.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+
+class Entry:
+    def __init__(self, line):
+        pieces = line.split()
+        self.low, self.high = (int(c) for c in pieces[0].split("-"))
+        self.char = pieces[1][0]
+        self.password = pieces[-1]
+
+
+def part1(entry):
+    count = sum(1 if c == entry.char else 0 for c in entry.password)
+    return count >= entry.low and count <= entry.high
+
+
+def part2(entry):
+    pos1_match = entry.password[entry.low - 1] == entry.char
+    pos2_match = entry.password[entry.high - 1] == entry.char
+    return pos1_match != pos2_match
+
+
+def run(validate, entries):
+    valid_count = sum(1 if validate(entry) else 0 for entry in entries)
+    print(valid_count)
+
+
+if __name__ == "__main__":
+    from input import day02
+
+    entries = [Entry(line) for line in day02.split("\n")]
+    run(part1, entries)
+    run(part2, entries)
