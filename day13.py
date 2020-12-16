9f758561e9672402b59cdb9df3115ef0d62a48f3
Cameron Spickert <cameron@checkhq.com>
2020-12-15 19:08:06 -0500

    Refactor day 13 part 1

--- day13.py
+++ day13.py
@@ -6,12 +6,18 @@ from base import BaseSolution
 class Solution(BaseSolution):
     def load_data(self, input):
         lines = input.splitlines()
-        return (int(lines[0]), sorted(int(i) for i in lines[1].split(",") if i != "x"))
+        return (
+            int(lines[0]),
+            [int(i) if i.isdigit() else 0 for i in lines[1].split(",")],
+        )
 
     def part1(self, args):
         timestamp, bus_ids = args
         bus_id, bus_timestamp = min(
-            ((bus_id, math.ceil(timestamp / bus_id) * bus_id) for bus_id in bus_ids),
+            (
+                (bus_id, math.ceil(timestamp / bus_id) * bus_id)
+                for bus_id in filter(None, bus_ids)
+            ),
             key=lambda pair: pair[1],
         )
         return bus_id * (bus_timestamp - timestamp)
