f9dc70cf7e6db749f0d9b0e51509e1ae6ed42b08
Cameron Spickert <cameron@checkhq.com>
2020-12-19 19:02:18 -0500

    Add day 13 part 2

--- day13.py
+++ day13.py
@@ -21,3 +21,20 @@ class Solution(BaseSolution):
             key=lambda pair: pair[1],
         )
         return bus_id * (bus_timestamp - timestamp)
+
+    def part2(self, args):
+        """
+        Couldn't quite get this one on my own. Solution based on:
+        https://gist.github.com/joshbduncan/65f810fe821c7a3ea81a1f5a444ea81e
+        """
+        _, bus_ids = args
+        timestamp = 0
+        step = 1
+        offsets_and_bus_ids = [
+            (offset, bus_id) for offset, bus_id in enumerate(bus_ids) if bus_id
+        ]
+        for offset, bus_id in offsets_and_bus_ids:
+            while (timestamp + offset) % bus_id != 0:
+                timestamp += step
+            step *= bus_id
+        return timestamp
