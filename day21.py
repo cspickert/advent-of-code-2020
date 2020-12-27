cff9a1262e047a263299e5991369b8179a9d7e2b
Cameron Spickert <cameron@checkhq.com>
2020-12-27 18:30:33 -0500

    Add day 21 part 2

--- day21.py
+++ day21.py
@@ -12,6 +12,48 @@ class Solution(BaseSolution):
         ]
 
     def part1(self, data):
+        return sum(
+            ingredient in ingredient_list
+            for ingredient in self.find_non_allergens(data)
+            for ingredient_list, _ in data
+        )
+
+    def part2(self, data):
+        return self.find_allergens(data)
+
+    def find_non_allergens(self, data):
+        return {
+            ingredient
+            for ingredient, allergens in self.find_possible_allergens(data).items()
+            if not allergens
+        }
+
+    def find_allergens(self, data):
+        mapping = dict(
+            (ingredient, allergens)
+            for ingredient, allergens in self.find_possible_allergens(data).items()
+            if allergens
+        )
+        while True:
+            updated = False
+            for ingredient, allergens in mapping.items():
+                if len(allergens) == 1:
+                    for other_ingredient in mapping:
+                        if other_ingredient != ingredient and mapping[
+                            other_ingredient
+                        ].intersection(allergens):
+                            mapping[other_ingredient] -= allergens
+                            updated = True
+            if not updated:
+                break
+        mapping = {
+            allergen: ingredient
+            for ingredient in mapping
+            for allergen in mapping[ingredient]
+        }
+        return ",".join(mapping[allergen] for allergen in sorted(mapping))
+
+    def find_possible_allergens(self, data):
         all_allergens = set()
         all_ingredients = set()
 
@@ -28,14 +70,4 @@ class Solution(BaseSolution):
                 if ingredient not in ingredient_list:
                     possible_allergens[ingredient] -= allergen_list
 
-        non_allergens = {
-            ingredient
-            for ingredient in possible_allergens
-            if not possible_allergens[ingredient]
-        }
-
-        return sum(
-            ingredient in ingredient_list
-            for ingredient in non_allergens
-            for ingredient_list, _ in data
-        )
+        return possible_allergens
